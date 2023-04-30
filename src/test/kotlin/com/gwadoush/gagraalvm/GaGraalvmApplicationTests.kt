5a7ebbbd9aff848feb33798d0413aaf0161d4914
Guillaume MOREL <guillaume.morel.dev@gmail.com>
2023-04-30 22:53:48 +0200

    Add spring app as hello world

---
+++ src/test/kotlin/com/gwadoush/gagraalvm/GaGraalvmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gwadoush.gagraalvm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GaGraalvmApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
